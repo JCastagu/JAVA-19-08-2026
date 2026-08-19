20e390880f8582885b4fefb457caafd3d84abf7a
JCastagu <juanes121208@gmail.com>
2026-08-19 13:14:27 -0500

    realice el segundo ejercicio

---
+++ src/Ejercicio2.java
@@ -0,0 +1,8 @@
+public class Ejercicio2 {
+    public static void main(String[] args){
+        saludarEstudiante("Juan", 18);
+    }
+    public static void saludarEstudiante(String nombre, int edad){
+        System.out.println("Hola " + nombre + ", tienes " + edad + " años. Bienvenido al instituto!");
+    }
+}
