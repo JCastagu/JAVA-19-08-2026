4191e4781a35317d96d32c2f24caca1bee51e681
JCastagu <juanes121208@gmail.com>
2026-08-19 13:25:08 -0500

    realice el ejercicio3

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Ejercicio3 {
+    public static void main(String[] args){
+        double area = calcularAreaRectangulo(3.1, 4);
+        System.out.println("El area del rectangulo es " + area);
+
+    }
+    public static double calcularAreaRectangulo(double base, double altura){
+        return base * altura;
+    }
 }
